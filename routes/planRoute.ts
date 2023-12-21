7e70ec4bb7c80c2104e324fd38a04c46dfc8b396
geeknk <ernitish26@gmail.com>
2023-12-21 14:37:01 +0530

    added swagger functionality

--- routes/planRoute.ts
+++ routes/planRoute.ts
@@ -3,6 +3,6 @@ export const planRouter = express.Router();
 import * as controller from "../controllers/planController";
 import * as mid from "../middlewares/userMiddle";
 
-planRouter.post("/add", mid.isOperator, controller.addPlans)
-planRouter.get("/all", controller.allPlans);
-planRouter.delete("/delete/:id", mid.isOperator, controller.deletePlan);
+planRouter.post("/add",mid.checkAuth, mid.isOperator, controller.addPlans)
+planRouter.get("/all", mid.checkAuth, controller.allPlans);
+planRouter.delete("/delete/:id",mid.checkAuth, mid.isOperator, controller.deletePlan);
